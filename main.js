a5baf353e37dbdaa0de7edb30c4d32f746d821cc
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-04-08 18:36:20 +0200

    add script

---
+++ main.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    //Slider
+
+    //References
+    var prevBtn = $("div.prev");
+    var nextBtn = $("div.next");
+
+    //On Click
+    //Previous
+    prevBtn.click(function(){
+        nextPrevSlide("prev");
+    });
+
+    //Next
+    nextBtn.click(function() {
+        nextPrevSlide("next");
+    });
+
+    //On Keydown
+    //Previous
+    $(document).keydown(function(event){
+        if(event.keyCode == 37) {
+            nextPrevSlide("prev");
+        } else if(event.keyCode == 39) {
+            nextPrevSlide("next");
+        }
+    });
+
+//End doc ready
+});
+
+/**********************************************
+ * FUNCTIONS
+***********************************************/
+
+//Carousel
+function nextPrevSlide(direction) {
+    var activeImg = $(".images img.active");
+    var activeCircle = $(".nav i.active");
+
+    //Reset
+    activeImg.removeClass("active");
+    activeCircle.removeClass("active");
+    
+    if(direction === "prev") {
+        if(activeImg.hasClass("first")) {
+            $(".images img.last").addClass("active")
+            $(".nav i.last").addClass("active")
+        }
+        activeImg.prev().addClass("active");
+        activeCircle.prev().addClass("active");
+
+    } else if(direction === "next") {
+        if(activeImg.hasClass("last")) {
+            $(".images img.first").addClass("active");
+            $(".nav i.first").addClass("active");
+        }
+        activeImg.next().addClass("active");
+        activeCircle.next().addClass("active");
+    }
+};
